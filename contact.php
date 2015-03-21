322a0cd7dbca001b22235f371205631201bd3645
Naveed Ahmed <inaveedahmed@gmail.com>
2015-03-21 22:22:00 +0500

    First Inn

---
+++ contact.php
@@ -0,0 +1,45 @@
+<?php 
+	if( true || $_POST)
+	{
+		require_once 'lib/purl_.php'; //curl library
+		$data  	= $_REQUEST;
+		$name  	= isset($data["name"])?$data["name"]:errme("Please enter your name");
+		$email 	= isset($data["email"])?$data["email"]:errme("Please enter your email");
+		$msg	= isset($data["comments"])?$data["comments"]:errme("Please enter your comments");
+
+		$compose = "Mr/Ms ".$name." with email: ".$email." Sent the following message: \n\r".$msg;
+		
+		// mailer data.
+		$dt = array( 
+			'from' 		=> 'Web Mail <mail@inaveed.com>',
+			'to'		=> 'Web Mail <mail@inaveed.com>',
+			'subject'	=> $name.' through inaveed.com web form',
+			'text'		=> $compose
+		);
+
+		$ch = curl_init();
+
+		curl_setopt_array($ch, array(
+		    CURLOPT_HTTPAUTH		=> CURLAUTH_BASIC,
+			CURLOPT_USERPWD 		=> 'api:key-c6dbb8764614a4ac956340e667b49d7b',
+		    CURLOPT_RETURNTRANSFER 	=> 1,
+		    CURLOPT_CUSTOMREQUEST	=> 'POST',
+		    CURLOPT_URL				=> 'https://api.mailgun.net/v2/inaveed.com/messages',
+			CURLOPT_POSTFIELDS 		=> implode('&', $dt)
+		));
+
+		$result = curl_exec($ch);
+
+		curl_close($ch);
+
+		print_r($result);
+
+		echo "Message successfully sent!";
+	}
+
+	function errme($v)
+	{
+		echo $v;
+		die();
+	}
+?>
